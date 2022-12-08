d195961d9d481352f52ad4242967bc694f3701e9
ib88 <imefire@gmail.com>
2022-12-08 11:15:39 +0100

    editing the .gitignore to resolve the conflicts

--- index.js
+++ index.js
@@ -44,9 +44,15 @@ const PORT = config.PORT;
 
 app.use(express.json());
 //app.use("/api", router);
+
+app.get('/', (req, res) => {
+  res.send('flight/flightOffer')
+})
 app.use("/api", hotelRouter);
 app.use("/flight", flightRouter);
 app.use("/Moqflight", MoqflightRouter);
+app.use("/", flightRouter);
+
 
 
 app.listen(PORT, () => {

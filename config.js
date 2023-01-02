9c204601ed31bdfee6cc82f038061252a1d5c8e8
ib88 <imefire@gmail.com>
2023-01-02 19:28:48 +0100

    renamed the stripe env varialbes again.

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   API_SECRET: process.env.API_SECRET,
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   PORT: process.env.PORT,
-  STRIPE_PUBLISHABLE_KEY: process.env.PUBLISHABLE_KEY,
-  STRIPE_SECRET_KEY: process.env.SECRET_KEY
+  STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+  STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY
 
 };

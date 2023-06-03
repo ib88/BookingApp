743058dc79b6edd626fb8fb4e328fb662815b17a
ib88 <imefire@gmail.com>
2023-06-03 10:52:36 +0100

    removed one of the imports from ImAdeusHotelRepo from hote_controller

--- controllers/Hotel_controller.ts
+++ controllers/Hotel_controller.ts
@@ -3,7 +3,7 @@
 const { AmadeusHotelRepo, AmadeusMockRepo } = require("../Repositories/IAmadeusHotelRepo");
 import { ObjectMapper } from "jackson-js";
 import { hotelInfos, hotelOffer } from "../Models/hotelOffer";
-import { AmadeusHotelMockRepo } from "../Repositories/IAmadeusHotelRepo";
+// import { AmadeusHotelMockRepo } from "../Repositories/IAmadeusHotelRepo";
 const { API_KEY, API_SECRET } = require("../config");
 const Amadeus = require("amadeus");
 const express = require("express");
@@ -32,7 +32,7 @@ const amadeus = new Amadeus({
 
 const objectMapper = new ObjectMapper();
 const amadeusHotelRepo = new AmadeusHotelRepo();
-const amadeusMockHotelRepo = new AmadeusHotelMockRepo();
+// const amadeusMockHotelRepo = new AmadeusHotelMockRepo();
 
 // Location search suggestions
 router.get(`/autosuggest`, async (req:any, res:any) => {
